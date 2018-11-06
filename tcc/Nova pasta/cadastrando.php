823a3d8e1a9096bf603c179bfb451e5a143fc770
leandro <lemccunha@gmail.com>
2018-11-06 12:55:47 -0300

    .

---
+++ tcc/Nova pasta/cadastrando.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastrando</title>
+</head>
+<body>
+
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$banco="cadastro";
+$conexao=mysqli_connect($host, $user, $pass) or die(mysqli_error());
+mysqli_select_db($conexao,$banco) or die(mysqli_error());
+?>
+
+
+<?php
+$nome=$_POST['nome'];
+$usuario=$_POST['usuario'];
+$senha=$_POST['senha'];
+$confirmasenha=$_POST['confirmasenha'];
+$email=$_POST['email'];
+$sql=mysqli_query($conexao," INSERT INTO usuarios(nome, usuario, senha, confirmasenha, email) VALUES ('$nome','$usuario','$senha',
+	'$confirmasenha', '$email')");
+echo "<center><h1>Cadastro realizado com sucesso</h1></center>";
+mysqli_close($conexao);
+?>
+</body>
+</html>﻿
