c1a365e3207ea9bb80683c706944654ece9c2c31
leandro machado <lemccunha@gmail.com>
2018-11-12 20:56:20 -0200

    up

--- tcc/controladores/sair.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  session_start();
-
-  unset($_SESSION['emailUsuarioLogado']);
-  header('Location: ../index.php');
-
-?>
--- tcc/script.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-    var option_value = document.getElementById("numbers").value;
-    if (option_value == "3") {
-        alert("Hai !");
-    }
-}
