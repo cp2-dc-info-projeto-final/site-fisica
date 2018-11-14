3ac66eae824ddebd078378fc6db6585b55d30fa7
Gustavo <Kishinzzz@hotmail.com>
2018-11-14 16:53:48 -0300

    14/10 - 17:55
    14/10 - 17:55
    Download das Listas
    Mecânica feita até agora

---
+++ tcc/AssuntosF/Mecânica.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>exercicios</title>
+<link rel="stylesheet" type="text/css" href="styleExer.css">
+Listas de Mecânica:
+
+<a href="Exercícios/2Ano/Mecânica/Vet.pdf">2ª série Int - Fis  - Lista 04 - Vetores - 
+pdf</a>
+    <a href="Exercícios/2Ano/Mecânica/DinamicaeL.pdf">2ª série Int - Fis  - Lista 05 - Dinâmica e Leis de Newton</a>
+    <a href="Exercícios/2Ano/Mecânica/TeE.pdf">2ª série Int - Fis  - Lista 06 - Trabalho e Energia</a>
+    <a href="Exercícios/2Ano/Mecânica/IeQ.pdf">2ª série Int - Fis  - Lista 07 - Impulso e Quantidade de movimento</a>
+    <a href="Exercícios/2Ano/Mecânica/Rev09.pdf">2ª série Int - Fis  - Lista 09 - Revisão</a>
+
+  
+
+</head>
+<body>
+</body>
+</html>
---
+++ tcc/AssuntosF/Termologia.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>exercicios</title>
+<link rel="stylesheet" type="text/css" href="styleExer.css">
+Listas de Mecânica:
+
+<a href="https://www.google.com.br">2ª série Int - Fis  - Lista 04 - Vetores - 
+pdf</a>
+    <a href="Exercícios/2Ano/Mecânica/DinamicaeL.pdf">2ª série Int - Fis  - Lista 05 - Dinâmica e Leis de Newton</a>
+    <a href="https://www.google.com.br">2ª série Int - Fis  - Lista 06 - Trabalho e Energia</a>
+    <a href="https://www.google.com.br">2ª série Int - Fis  - Lista 07 - Impulso e Quantidade de movimento</a>
+    <a href="https://www.google.com.br">2ª série Int - Fis  - Lista 09 - Revisão</a>
+
+  
+
+</head>
+<body>
+</body>
+</html>
