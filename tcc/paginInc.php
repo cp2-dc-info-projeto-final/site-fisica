c04931684266cbb5141c290932c3a940f0a6d18b
mcwytin <wyctorhector@gmail.com>
2018-11-20 23:18:54 -0200

    .

--- tcc/paginInc.php
+++ tcc/paginInc.php
@@ -1,25 +1,5 @@
-<?php
- 
-require_once('modelo/tabelausuario.php');
 
-session_start();
-
-  if (array_key_exists('username', $_SESSION) &&
-      array_key_exists('idUsuárioConectado', $_SESSION))
-  {
-   
-    $user_name = $_SESSION['username'];
-    $id = $_SESSION['idUsuárioConectado'];
-    $usuario = BuscaUsuarioPorId($id);
-    echo $user_name;
-  }
-  else
-  {
-    $usuario = null;
-    header('Location:login.php');
-  }
-?>
-     <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
@@ -31,7 +11,29 @@ session_start();
 
   <body>
     <div id="D1">	
-        
+        <?php
+ 
+require_once('modelo/tabelausuario.php');
+
+session_start();
+
+  if (array_key_exists('username', $_SESSION) &&
+      array_key_exists('idUsuárioConectado', $_SESSION))
+  {
+   
+    $user_name = $_SESSION['username'];
+    echo $user_name;
+
+    $id = $_SESSION['idUsuárioConectado'];
+    $usuario = BuscaUsuarioPorId($id);
+  }
+  else
+  {
+    header('Location:loginAluno.php');
+  }
+?>
+        <div class="logo"><img src="images/pedroii.png" class="escola" ></div>
+    
       <div class="prof">
       <br>
      <br>
@@ -44,7 +46,6 @@ session_start();
     </div>
 	        
         <ul>
-          <li><a href="paginInc.php">Home</a></li>
           <li><a href="formulas.php">Formulas</a></li>
           <li><a href="Exercicios.php">Exercicios</a></li>
           <li><a href="videos.php">Videos</a></li>
@@ -66,27 +67,31 @@ session_start();
     </div>
 
     <div id="D3">
-        
+        <h1>ATENÇAO</h1><br>
+      Este é um site com o intuito de ajudar os alunos do Colégio Pedro II na matéria de física.<br>
+      (OBS.: Ainda estamos em fase de desenvolvimento.)    
     </div>
 
   <div id="D4">
+      <h1><a href="exercicios.php">Exercicios</a></h1><br>
+      Poderá visualizar os exercicios de cada assunto abordado em sala e baixá-los.
      
   </div>
 
   <div id="D5">
-      
+      <h1><a href="videos.php">Videos</a></h1><br>
+      Poderá assistir videos de experimentos.
   </div>
 
   <div id="D6">
-     
+      <h1><a href="formulas.php">Formulas</a></h1><br>
+      Poderá visualizar algumas formulas de cada assunto abordado em sala.
   </div>
 
-  <div id="D7">
-      
-  </div>
+ 
 
   <div id="D8">
-      <p></p>
+      © Copyright
   </div>
 
 </body>
