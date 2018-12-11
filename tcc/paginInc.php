e1cf1099fb04ff0a7a1664cc5b30acd840a9bb3f
joaovictorsxp <jvsxp.123@hotmail.com>
2018-12-11 10:45:38 -0300

    .2

--- tcc/paginInc.php
+++ tcc/paginInc.php
@@ -2,7 +2,58 @@
 <!DOCTYPE html>
 <html>
 <style>
+@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300);
 
+body,html{
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+    font-family: "Source Sans Pro" sans-serif;
+}
+.button__holder{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    background-color: #1a1a1a;
+    &:after{
+        content: "";
+        position: absolute;
+        z-index: 0;
+        top: 0; right: 0; bottom: 0; left: 0;
+        background: rgba(#1a1a1a, 0.7);
+    }
+    h2{
+        margin: {right: 1em;}
+        text-align: center;
+        color: #8A3B58;
+    }
+}
+.plus{
+    position: relative;
+    z-index: 1;
+    width: 80px;
+    height: 80px;
+    color: #1a1a1a;
+    background: #ed1c5b;
+    border-radius: 50%;
+    border: none;
+    transition: box-shadow 400ms cubic-bezier(.2,0,.7,1), transform 200ms cubic-bezier(.2,0,.7,1);
+    &:after{
+        content: "+";
+        font-size: 2.5em;
+        line-height: 1.1em;
+    }
+    &:hover{
+        transform: rotate(45deg);
+        box-shadow: 0 0 1px 15px rgba(#8A3B58, 0.4),
+        0 0 1px 30px rgba(#8A3B58, 0.1),
+        0 0 1px 45px rgba(#8A3B58, 0.1);
+    }
+}
 .d4 {
 background-color: black;
 
@@ -20,9 +71,9 @@ background-color: black;
 </head>
 
   <body>
-    <div id="D1">
+    <div id="D1"> 
         <?php
-
+ 
 require_once('modelo/tabelausuario.php');
 
 session_start();
@@ -30,10 +81,9 @@ session_start();
   if (array_key_exists('username', $_SESSION) &&
       array_key_exists('idUsuárioConectado', $_SESSION))
   {
-
+   
     $user_name = $_SESSION['username'];
-
-
+    echo $user_name;
 
     $id = $_SESSION['idUsuárioConectado'];
     $usuario = BuscaUsuarioPorId($id);
@@ -43,30 +93,26 @@ session_start();
     header('Location:loginAluno.php');
   }
 ?>
-        <div class="logo"><img src="fisi.jpg" class="escola" ></div>
-
+        <div class="logo"><img src="images/hig.jpg" class="escola" ></div>
+    
       <div class="prof">
-        <br>
-        <div class="username"> <?php echo $user_name; ?> </div>
       <br>
      <br>
           <?php if($usuario['matricula'] != null){ ?>
-             <a class = "linkpi" href="cadastroprof.php"> <button id="CadButton" > Fazer o Cadastro </button> </a>
-
-                   <?php } ?>
-
+             <a class = "linkpi" href="Cadastro.php"> <button id="CadButton" > Fazer o Cadastro </button> </a>
+                   <?php } ?> 
       <br>
             <a href="controlador/sair.php"> <button id="Button" > Sair </button> </a>
-      </div>
+      </div>        
     </div>
-
+          
         <ul>
           <li><a href="formulas.php">Formulas</a></li>
           <li><a href="Exercicios.php">Exercicios</a></li>
           <li><a href="videos.php">Videos</a></li>
-
+         
         </ul>
-
+    
           <div class="Slides">
 
       <slider>
@@ -78,19 +124,19 @@ session_start();
     </div>
 
     <div id="D2">
-
+        
     </div>
 
     <div id="D3">
         <h1>ATENÇÃO</h1>
       Este é um site com o intuito de ajudar os alunos do Colégio Pedro II na matéria de física.<br>
-      (OBS.: Ainda estamos em fase de desenvolvimento.)
+      (OBS.: Ainda estamos em fase de desenvolvimento.)    
     </div>
 
   <div id="D4">
       <h1><a href="exercicios.php">Exercicios</a></h1><br>
       Poderá visualizar os exercicios de cada assunto abordado em sala e baixá-los.
-
+     
   </div>
 
   <div id="D5">
@@ -103,7 +149,7 @@ session_start();
       Poderá visualizar algumas formulas de cada assunto abordado em sala.
   </div>
 
-
+ 
 
   <div id="D8">
       © Copyright
